965886987cafb4a3d16905d2a344a17d2bf9b55d
Alexandru-Emil Lupu <contact@alecslupu.ro>
2022-07-10 18:40:18 +0300

    Install Sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # authenticate :user, ->(u) { u.admin? } do
+    mount Sidekiq::Web, at: "/sidekiq"
+  # end
+
   # Defines the root path route ("/")
   # root "articles#index"
+
 end
